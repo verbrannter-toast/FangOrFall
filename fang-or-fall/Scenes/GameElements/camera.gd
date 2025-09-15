158d155050b3adf45efd46f38eb54cf3fdfaf98c
Erik Mettner <toast.tries.art@gmail.com>
2025-09-15 13:48:56 +0200

    Smooth Camera movement
    Added smoother movement to the follow camera

---
+++ fang-or-fall/Scenes/GameElements/camera.gd
@@ -0,0 +1,17 @@
+extends Camera2D
+
+var main_node
+var follow_speed := 5
+
+func _ready():
+	main_node = get_node("/root/Main")
+
+func _process(delta):
+	if main_node.snake.size() > 0:
+		var head_pos = main_node.snake[0].global_position
+		var dir = main_node.move_direction.normalized()
+		var offset = dir * 100
+
+		var target_pos = head_pos + offset
+
+		global_position = global_position.lerp(target_pos, delta * follow_speed)
