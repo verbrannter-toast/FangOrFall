66f14cd370419aa003faa8f54d40bc09e72527f4
Erik Mettner <toast.tries.art@gmail.com>
2025-10-17 17:07:01 +0200

    Test deployment for Railway

--- godot-project/Client/Game/Main.gd
+++ godot-project/Client/Game/Main.gd
@@ -1,23 +1,7 @@
 extends Node
 
 func _ready():
-	# Check command line arguments
-	var is_server = false
-	for arg in OS.get_cmdline_args():
-		if arg == "--server":
-			is_server = true
-			break
-	
-	# Also check feature tags
-	if OS.has_feature("dedicated_server"):
-		is_server = true
-	
-	# Load appropriate scene
-	if is_server:
-		print("=================================================")
-		print("STARTING DEDICATED SERVER")
-		print("=================================================")
-		get_tree().change_scene_to_file("res://Server/Server.tscn")
-	else:
-		print("Starting client...")
-		get_tree().change_scene_to_file("res://Client/Client.tscn")
+	print("=================================================")
+	print("STARTING DEDICATED SERVER")
+	print("=================================================")
+	get_tree().change_scene_to_file("res://Server/Server.tscn")
